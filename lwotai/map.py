92859a32fd042d59e310e0fb960cbd030a7bdb14
Andrew Swan <andrew.i.swan@gmail.com>
2017-08-13 17:04:07 +1000

    Simplified linking of countries on map

--- lwotai/map.py
+++ lwotai/map.py
@@ -2,224 +2,95 @@ from lwotai.country import Country
 from lwotai.governance import GOOD, FAIR
 
 
+def _get_countries(app):
+    """Returns the countries to add to the map"""
+    return [
+        Country(app, "Algeria/Tunisia", "Suni", "", None, False, 0, True, 2, schengen_link=True),
+        Country(app, "Afghanistan", "Shia-Mix", "", None, False, 0, False, 1),
+        Country(app, "Benelux", "Non-Muslim", "", GOOD, True, 0, False, 0),
+        Country(app, "Canada", "Non-Muslim", "", GOOD, False, 0, False, 0, schengen_link=True),
+        Country(app, "Caucasus", "Non-Muslim", "", FAIR, False, 0, False, 0),
+        Country(app, "Central Asia", "Suni", "", None, False, 0, False, 2),
+        Country(app, "China", "Non-Muslim", "", FAIR, False, 0, False, 0),
+        Country(app, "Eastern Europe", "Non-Muslim", "", GOOD, True, 0, False, 0),
+        Country(app, "Egypt", "Suni", "", None, False, 0, False, 3),
+        Country(app, "France", "Non-Muslim", "", GOOD, True, 2, False, 0),
+        Country(app, "Germany", "Non-Muslim", "", GOOD, True, 0, False, 0),
+        Country(app, "Gulf States", "Shia-Mix", "", None, False, 0, True, 3),
+        Country(app, "India", "Non-Muslim", "", GOOD, False, 0, False, 0),
+        Country(app, "Indonesia/Malaysia", "Suni", "", None, False, 0, True, 3),
+        Country(app, "Iran", "Iran", None, FAIR, False, 0, False, 0),
+        Country(app, "Iraq", "Shia-Mix", "", None, False, 0, True, 3),
+        Country(app, "Israel", "Non-Muslim", "Hard", GOOD, False, 0, False, 0),
+        Country(app, "Italy", "Non-Muslim", "", GOOD, True, 0, False, 0),
+        Country(app, "Jordan", "Suni", "", None, False, 0, False, 1),
+        Country(app, "Kenya/Tanzania", "Non-Muslim", "", FAIR, False, 0, False, 0),
+        Country(app, "Lebanon", "Shia-Mix", "", None, False, 0, False, 1, schengen_link=True),
+        Country(app, "Libya", "Suni", "", None, False, 0, True, 1, schengen_link=True),
+        Country(app, "Morocco", "Suni", "", None, False, 0, False, 2, schengen_link=True),
+        Country(app, "Pakistan", "Shia-Mix", "", None, False, 0, False, 2),
+        Country(app, "Philippines", "Non-Muslim", "", FAIR, False, 3, False, 0),
+        Country(app, "Russia", "Non-Muslim", "", FAIR, False, 0, False, 0, schengen_link=True),
+        Country(app, "Saudi Arabia", "Shia-Mix", "", None, False, 0, True, 3),
+        Country(app, "Scandinavia", "Non-Muslim", "", GOOD, True, 0, False, 0),
+        Country(app, "Serbia", "Non-Muslim", "", GOOD, False, 0, False, 0, schengen_link=True),
+        Country(app, "Somalia", "Suni", "", None, False, 0, False, 1),
+        Country(app, "Spain", "Non-Muslim", "", GOOD, True, 2, False, 0),
+        Country(app, "Sudan", "Suni", "", None, False, 0, True, 1),
+        Country(app, "Syria", "Suni", "", None, False, 0, False, 2),
+        Country(app, "Thailand", "Non-Muslim", "", FAIR, False, 0, False, 0),
+        Country(app, "Turkey", "Shia-Mix", "", None, False, 0, False, 2, schengen_link=True),
+        Country(app, "United Kingdom", "Non-Muslim", "", GOOD, False, 3, False, 0, schengen_link=True),
+        Country(app, "United States", "Non-Muslim", "Hard", GOOD, False, 0, False, 0, schengen_link=True),
+        Country(app, "Yemen", "Shia-Mix", "", None, False, 0, False, 1)
+    ]
+
+
 class Map(object):
-    """The map on the board"""
+    """The board map, i.e. the countries and the links between them"""
 
     def __init__(self, app):
-        self._map = {
-            "Algeria/Tunisia":
-                Country(app, "Algeria/Tunisia", "Suni", "", None, False, 0, True, 2, schengen_link=True),
-            "Afghanistan":
-                Country(app, "Afghanistan", "Shia-Mix", "", None, False, 0, False, 1),
-            "Benelux":
-                Country(app, "Benelux", "Non-Muslim", "", GOOD, True, 0, False, 0),
-            "Canada":
-                Country(app, "Canada", "Non-Muslim", "", GOOD, False, 0, False, 0, schengen_link=True),
-            "Caucasus":
-                Country(app, "Caucasus", "Non-Muslim", "", FAIR, False, 0, False, 0),
-            "Central Asia":
-                Country(app, "Central Asia", "Suni", "", None, False, 0, False, 2),
-            "China":
-                Country(app, "China", "Non-Muslim", "", FAIR, False, 0, False, 0),
-            "Eastern Europe":
-                Country(app, "Eastern Europe", "Non-Muslim", "", GOOD, True, 0, False, 0),
-            "Egypt":
-                Country(app, "Egypt", "Suni", "", None, False, 0, False, 3),
-            "France":
-                Country(app, "France", "Non-Muslim", "", GOOD, True, 2, False, 0),
-            "Germany":
-                Country(app, "Germany", "Non-Muslim", "", GOOD, True, 0, False, 0),
-            "Gulf States":
-                Country(app, "Gulf States", "Shia-Mix", "", None, False, 0, True, 3),
-            "India":
-                Country(app, "India", "Non-Muslim", "", GOOD, False, 0, False, 0),
-            "Indonesia/Malaysia":
-                Country(app, "Indonesia/Malaysia", "Suni", "", None, False, 0, True, 3),
-            "Iran":
-                Country(app, "Iran", "Iran", None, FAIR, False, 0, False, 0),
-            "Iraq":
-                Country(app, "Iraq", "Shia-Mix", "", None, False, 0, True, 3),
-            "Israel":
-                Country(app, "Israel", "Non-Muslim", "Hard", GOOD, False, 0, False, 0),
-            "Italy":
-                Country(app, "Italy", "Non-Muslim", "", GOOD, True, 0, False, 0),
-            "Jordan":
-                Country(app, "Jordan", "Suni", "", None, False, 0, False, 1),
-            "Kenya/Tanzania":
-                Country(app, "Kenya/Tanzania", "Non-Muslim", "", FAIR, False, 0, False, 0),
-            "Lebanon":
-                Country(app, "Lebanon", "Shia-Mix", "", None, False, 0, False, 1, schengen_link=True),
-            "Libya":
-                Country(app, "Libya", "Suni", "", None, False, 0, True, 1, schengen_link=True),
-            "Morocco":
-                Country(app, "Morocco", "Suni", "", None, False, 0, False, 2, schengen_link=True),
-            "Pakistan":
-                Country(app, "Pakistan", "Shia-Mix", "", None, False, 0, False, 2),
-            "Philippines":
-                Country(app, "Philippines", "Non-Muslim", "", FAIR, False, 3, False, 0),
-            "Russia":
-                Country(app, "Russia", "Non-Muslim", "", FAIR, False, 0, False, 0, schengen_link=True),
-            "Saudi Arabia":
-                Country(app, "Saudi Arabia", "Shia-Mix", "", None, False, 0, True, 3),
-            "Scandinavia":
-                Country(app, "Scandinavia", "Non-Muslim", "", GOOD, True, 0, False, 0),
-            "Serbia":
-                Country(app, "Serbia", "Non-Muslim", "", GOOD, False, 0, False, 0, schengen_link=True),
-            "Somalia":
-                Country(app, "Somalia", "Suni", "", None, False, 0, False, 1),
-            "Spain":
-                Country(app, "Spain", "Non-Muslim", "", GOOD, True, 2, False, 0),
-            "Sudan":
-                Country(app, "Sudan", "Suni", "", None, False, 0, True, 1),
-            "Syria":
-                Country(app, "Syria", "Suni", "", None, False, 0, False, 2),
-            "Thailand":
-                Country(app, "Thailand", "Non-Muslim", "", FAIR, False, 0, False, 0),
-            "Turkey":
-                Country(app, "Turkey", "Shia-Mix", "", None, False, 0, False, 2, schengen_link=True),
-            "United Kingdom":
-                Country(app, "United Kingdom", "Non-Muslim", "", GOOD, False, 3, False, 0, schengen_link=True),
-            "United States":
-                Country(app, "United States", "Non-Muslim", "Hard", GOOD, False, 0, False, 0, schengen_link=True),
-            "Yemen":
-                Country(app, "Yemen", "Shia-Mix", "", None, False, 0, False, 1)
-        }
+        self._map = {}
+        for country in _get_countries(app):
+            self._map[country.name] = country
 
-        # Canada
-        self._map["Canada"].links.append(self._map["United States"])
-        self._map["Canada"].links.append(self._map["United Kingdom"])
-        # United States
-        self._map["United States"].links.append(self._map["Canada"])
-        self._map["United States"].links.append(self._map["United Kingdom"])
-        self._map["United States"].links.append(self._map["Philippines"])
-        # United Kingdom
-        self._map["United Kingdom"].links.append(self._map["Canada"])
-        self._map["United Kingdom"].links.append(self._map["United States"])
-        # Serbia
-        self._map["Serbia"].links.append(self._map["Russia"])
-        self._map["Serbia"].links.append(self._map["Turkey"])
-        # Israel
-        self._map["Israel"].links.append(self._map["Lebanon"])
-        self._map["Israel"].links.append(self._map["Jordan"])
-        self._map["Israel"].links.append(self._map["Egypt"])
-        # India
-        self._map["India"].links.append(self._map["Pakistan"])
-        self._map["India"].links.append(self._map["Indonesia/Malaysia"])
-        # Russia
-        self._map["Russia"].links.append(self._map["Serbia"])
-        self._map["Russia"].links.append(self._map["Turkey"])
-        self._map["Russia"].links.append(self._map["Caucasus"])
-        self._map["Russia"].links.append(self._map["Central Asia"])
-        # Caucasus
-        self._map["Caucasus"].links.append(self._map["Russia"])
-        self._map["Caucasus"].links.append(self._map["Turkey"])
-        self._map["Caucasus"].links.append(self._map["Iran"])
-        self._map["Caucasus"].links.append(self._map["Central Asia"])
-        # China
-        self._map["China"].links.append(self._map["Central Asia"])
-        self._map["China"].links.append(self._map["Thailand"])
-        # Kenya/Tanzania
-        self._map["Kenya/Tanzania"].links.append(self._map["Sudan"])
-        self._map["Kenya/Tanzania"].links.append(self._map["Somalia"])
-        # Thailand
-        self._map["Thailand"].links.append(self._map["China"])
-        self._map["Thailand"].links.append(self._map["Philippines"])
-        self._map["Thailand"].links.append(self._map["Indonesia/Malaysia"])
-        # Philippines
-        self._map["Philippines"].links.append(self._map["United States"])
-        self._map["Philippines"].links.append(self._map["Thailand"])
-        self._map["Philippines"].links.append(self._map["Indonesia/Malaysia"])
-        # Morocco
-        self._map["Morocco"].links.append(self._map["Algeria/Tunisia"])
-        # Algeria/Tunisia
-        self._map["Algeria/Tunisia"].links.append(self._map["Morocco"])
-        self._map["Algeria/Tunisia"].links.append(self._map["Libya"])
-        # Libya
-        self._map["Libya"].links.append(self._map["Algeria/Tunisia"])
-        self._map["Libya"].links.append(self._map["Egypt"])
-        self._map["Libya"].links.append(self._map["Sudan"])
-        # Egypt
-        self._map["Egypt"].links.append(self._map["Libya"])
-        self._map["Egypt"].links.append(self._map["Israel"])
-        self._map["Egypt"].links.append(self._map["Sudan"])
-        # Sudan
-        self._map["Sudan"].links.append(self._map["Libya"])
-        self._map["Sudan"].links.append(self._map["Egypt"])
-        self._map["Sudan"].links.append(self._map["Kenya/Tanzania"])
-        self._map["Sudan"].links.append(self._map["Somalia"])
-        # Somalia
-        self._map["Somalia"].links.append(self._map["Sudan"])
-        self._map["Somalia"].links.append(self._map["Kenya/Tanzania"])
-        self._map["Somalia"].links.append(self._map["Yemen"])
-        # Jordan
-        self._map["Jordan"].links.append(self._map["Israel"])
-        self._map["Jordan"].links.append(self._map["Syria"])
-        self._map["Jordan"].links.append(self._map["Iraq"])
-        self._map["Jordan"].links.append(self._map["Saudi Arabia"])
-        # Syria
-        self._map["Syria"].links.append(self._map["Turkey"])
-        self._map["Syria"].links.append(self._map["Lebanon"])
-        self._map["Syria"].links.append(self._map["Jordan"])
-        self._map["Syria"].links.append(self._map["Iraq"])
-        # Central Asia
-        self._map["Central Asia"].links.append(self._map["Russia"])
-        self._map["Central Asia"].links.append(self._map["Caucasus"])
-        self._map["Central Asia"].links.append(self._map["Iran"])
-        self._map["Central Asia"].links.append(self._map["Afghanistan"])
-        self._map["Central Asia"].links.append(self._map["China"])
-        # Indonesia/Malaysia
-        self._map["Indonesia/Malaysia"].links.append(self._map["Thailand"])
-        self._map["Indonesia/Malaysia"].links.append(self._map["India"])
-        self._map["Indonesia/Malaysia"].links.append(self._map["Philippines"])
-        self._map["Indonesia/Malaysia"].links.append(self._map["Pakistan"])
-        # Turkey
-        self._map["Turkey"].links.append(self._map["Serbia"])
-        self._map["Turkey"].links.append(self._map["Russia"])
-        self._map["Turkey"].links.append(self._map["Caucasus"])
-        self._map["Turkey"].links.append(self._map["Iran"])
-        self._map["Turkey"].links.append(self._map["Syria"])
-        self._map["Turkey"].links.append(self._map["Iraq"])
-        # Lebanon
-        self._map["Lebanon"].links.append(self._map["Syria"])
-        self._map["Lebanon"].links.append(self._map["Israel"])
-        # Yemen
-        self._map["Yemen"].links.append(self._map["Saudi Arabia"])
-        self._map["Yemen"].links.append(self._map["Somalia"])
-        # Iraq
-        self._map["Iraq"].links.append(self._map["Syria"])
-        self._map["Iraq"].links.append(self._map["Turkey"])
-        self._map["Iraq"].links.append(self._map["Iran"])
-        self._map["Iraq"].links.append(self._map["Gulf States"])
-        self._map["Iraq"].links.append(self._map["Saudi Arabia"])
-        self._map["Iraq"].links.append(self._map["Jordan"])
-        # Saudi Arabia
-        self._map["Saudi Arabia"].links.append(self._map["Jordan"])
-        self._map["Saudi Arabia"].links.append(self._map["Iraq"])
-        self._map["Saudi Arabia"].links.append(self._map["Gulf States"])
-        self._map["Saudi Arabia"].links.append(self._map["Yemen"])
-        # Gulf States
-        self._map["Gulf States"].links.append(self._map["Iran"])
-        self._map["Gulf States"].links.append(self._map["Pakistan"])
-        self._map["Gulf States"].links.append(self._map["Saudi Arabia"])
-        self._map["Gulf States"].links.append(self._map["Iraq"])
-        # Pakistan
-        self._map["Pakistan"].links.append(self._map["Iran"])
-        self._map["Pakistan"].links.append(self._map["Afghanistan"])
-        self._map["Pakistan"].links.append(self._map["India"])
-        self._map["Pakistan"].links.append(self._map["Gulf States"])
-        self._map["Pakistan"].links.append(self._map["Indonesia/Malaysia"])
-        # Afghanistan
-        self._map["Afghanistan"].links.append(self._map["Central Asia"])
-        self._map["Afghanistan"].links.append(self._map["Pakistan"])
-        self._map["Afghanistan"].links.append(self._map["Iran"])
-        # Iran
-        self._map["Iran"].links.append(self._map["Central Asia"])
-        self._map["Iran"].links.append(self._map["Afghanistan"])
-        self._map["Iran"].links.append(self._map["Pakistan"])
-        self._map["Iran"].links.append(self._map["Gulf States"])
-        self._map["Iran"].links.append(self._map["Iraq"])
-        self._map["Iran"].links.append(self._map["Turkey"])
-        self._map["Iran"].links.append(self._map["Caucasus"])
+        self._link_country("Afghanistan", "Central Asia", "Iran", "Pakistan")
+        self._link_country("Algeria/Tunisia", "Libya", "Morocco")
+        self._link_country("Canada", "United Kingdom", "United States")
+        self._link_country("Caucasus", "Central Asia", "Iran", "Russia", "Turkey")
+        self._link_country("Central Asia", "Afghanistan", "Caucasus", "China", "Iran", "Russia")
+        self._link_country("China", "Central Asia", "Thailand")
+        self._link_country("Egypt", "Israel", "Libya", "Sudan")
+        self._link_country("Gulf States", "Iran", "Iraq", "Pakistan", "Saudi Arabia")
+        self._link_country("India", "Indonesia/Malaysia", "Pakistan")
+        self._link_country("Indonesia/Malaysia", "India", "Pakistan", "Philippines", "Thailand")
+        self._link_country("Iran", "Afghanistan", "Caucasus", "Central Asia", "Gulf States", "Iraq", "Pakistan",
+                           "Turkey")
+        self._link_country("Iraq", "Gulf States", "Iran", "Jordan", "Saudi Arabia", "Syria", "Turkey")
+        self._link_country("Israel", "Egypt", "Jordan", "Lebanon")
+        self._link_country("Jordan", "Iraq", "Israel", "Saudi Arabia", "Syria")
+        self._link_country("Kenya/Tanzania", "Somalia", "Sudan")
+        self._link_country("Lebanon", "Israel", "Syria")
+        self._link_country("Libya", "Algeria/Tunisia", "Egypt", "Sudan")
+        self._link_country("Morocco", "Algeria/Tunisia")
+        self._link_country("Pakistan", "Afghanistan", "Gulf States", "India", "Indonesia/Malaysia", "Iran")
+        self._link_country("Philippines", "Indonesia/Malaysia", "Thailand", "United States")
+        self._link_country("Russia", "Caucasus", "Central Asia", "Serbia", "Turkey")
+        self._link_country("Saudi Arabia", "Gulf States", "Iraq", "Jordan", "Yemen")
+        self._link_country("Serbia", "Russia", "Turkey")
+        self._link_country("Somalia", "Kenya/Tanzania", "Sudan", "Yemen")
+        self._link_country("Sudan", "Egypt", "Kenya/Tanzania", "Libya", "Somalia")
+        self._link_country("Syria", "Iraq", "Jordan", "Lebanon", "Turkey")
+        self._link_country("Thailand", "China", "Indonesia/Malaysia", "Philippines")
+        self._link_country("Turkey", "Caucasus", "Iran", "Iraq", "Russia", "Serbia", "Syria")
+        self._link_country("United Kingdom", "Canada", "United States")
+        self._link_country("United States", "Canada", "Philippines", "United Kingdom")
+        self._link_country("Yemen", "Saudi Arabia", "Somalia")
+
+    def _link_country(self, country, *other_countries):
+        """Links the first named country to the other named countries"""
+        for other_country in other_countries:
+            self._map[country].links.append(self._map[other_country])
 
     def country_names(self):
         """Returns the names of all countries on this map"""
--- tests/test_map.py
+++ tests/test_map.py
@@ -1,11 +1,70 @@
 from labyrinth_test_case import LabyrinthTestCase
-from lwotai.labyrinth import Labyrinth
+from lwotai.map import Map
 
 
-class Map(LabyrinthTestCase):
+class MapTest(LabyrinthTestCase):
 
-    def testDeck(self):
-        app = Labyrinth(1, 1, self.set_up_test_scenario)
-        for country in app.get_countries():
-            for link in country.links:
-                self.assertTrue(country in link.links)
+    @staticmethod
+    def create_test_map():
+        return Map(None)
+
+    def test_all_links_are_reciprocal(self):
+        game_map = self.create_test_map()
+        for country in game_map.countries():
+            for linked_country in country.links:
+                self.assertTrue(country in linked_country.links,
+                                "%s is not in the links of %s" % (country, linked_country))
+
+    def test_country_names(self):
+        # Set up
+        game_map = self.create_test_map()
+
+        # Invoke
+        country_names = game_map.country_names()
+
+        # Assert
+        self.assertEqual(38, len(country_names))
+        self.assertTrue("France" in country_names)
+
+    def test_find_by_partial_miscased_name(self):
+        # Set up
+        game_map = self.create_test_map()
+
+        # Invoke
+        country_names = game_map.find_by_name("f stat")
+
+        # Assert
+        self.assertEqual(["Gulf States"], country_names)
+
+    def test_find_by_false_predicate(self):
+        # Set up
+        game_map = self.create_test_map()
+
+        # Invoke
+        unlinked_countries = game_map.find(lambda c: not c.schengen and not c.links)
+
+        # Assert
+        self.assertEqual([], unlinked_countries, "Found %s" % unlinked_countries)
+
+    def test_find_by_partially_true_predicate(self):
+        # Set up
+        game_map = self.create_test_map()
+
+        # Invoke
+        u_countries = game_map.find(lambda c: c.name.startswith("U"))
+
+        # Assert
+        u_country_names = [country.name for country in u_countries]
+        self.assertEqual(["United States", "United Kingdom"], u_country_names)
+
+    def test_links_from_afghanistan(self):
+        # Set up
+        game_map = self.create_test_map()
+
+        # Invoke
+        links = game_map.get("Afghanistan").links
+
+        # Assert
+        link_names = [c.name for c in links]
+        expected_link_names = ["Central Asia", "Iran", "Pakistan"]
+        self.assertEqual(expected_link_names, link_names)
