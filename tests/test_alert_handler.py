11358cb77acd1f0b94ad798af1e405e035f83d49
Andrew Swan <andrew.i.swan@gmail.com>
2017-09-12 12:47:02 +1000

    Fixed alert test

--- tests/test_alert_handler.py
+++ tests/test_alert_handler.py
@@ -5,13 +5,13 @@ from lwotai.labyrinth import Labyrinth
 class AlertHandlerTest(LabyrinthTestCase):
     """Test Alert"""
 
-    def test_alert(self):
+    def test_alert_reduces_plot_count_by_one(self):
         app = Labyrinth(1, 1, self.set_up_test_scenario)
         iraq = app.map.get("Iraq")
         self.assertEqual(iraq.plots, 2)
-        app.handle_alert("Iraq")
+        app.alert_plot("Iraq")
         self.assertEqual(iraq.plots, 1)
-        app.handle_alert("Iraq")
+        app.alert_plot("Iraq")
         self.assertEqual(iraq.plots, 0)
-        app.handle_alert("Iraq")
-        self.assertEqual(iraq.plots, 0)
+        with self.assertRaises(AssertionError):
+            app.alert_plot("Iraq")
